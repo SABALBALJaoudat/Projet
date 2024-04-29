3b72ed63ec1ccd80548070661a9cb4686c5b746e
SABALBALJaoudat <jaoudat.joudinho@hotmail.fr>
2024-04-29 14:33:53 +0200

    Ajout composant Nav, Search et Section + correction tailwind

--- clonage-airbnb/src/App.tsx
+++ clonage-airbnb/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-      <h1 className="bg-red-500">Clonage Airbnb</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
